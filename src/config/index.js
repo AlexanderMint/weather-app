a2aed9b3e8ed4b64877a9855416215609858ab36
Sergey.Illarionov <easthunch@gmail.com>
2017-07-13 11:59:42 +0300

    refactoring

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable import/prefer-default-export */
 export const api = {
+  key: 'e8c783c0b10b1df07e1af092d72fef7e',
   paths: {
     '': '',
   }
 };
+/* eslint-enable import/prefer-default-export */
--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import { pendingTasksReducer as pendingTasks } from 'react-redux-spinner';
 
 
 export default combineReducers({
-  routing: routerReducer,
-  pendingTasks
+  routing: routerReducer
 });
