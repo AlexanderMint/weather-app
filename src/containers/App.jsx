f03838413b09ce56a3c8c3be3a6c34ef5f34595b
Sergey.Illarionov <easthunch@gmail.com>
2017-07-17 17:33:01 +0300

    add: favicons

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -16,6 +16,7 @@ class App extends React.Component {
     return (
       <div className="app-box">
         <AppBar
+          className="header"
           title="Weather App"
           iconElementLeft={<span />}
         />
