50c32d340df588db4cd071d6d2f9f9ff5b56aadc
Sergey.Illarionov <easthunch@gmail.com>
2017-07-20 10:51:59 +0300

    add: style for forecast for 7 days

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -32,6 +32,7 @@ class CityCard extends React.Component {
               <span className="city-card__time">PM<span>12:40</span></span>
               <span className="city-card__date">FRI SEP.28</span>
             </div>
+            <div className="divider" />
             <div className="city-card__info clear-fix">
               <dl className="city-card__info-list">
                 <dt>Wind:</dt>
@@ -49,6 +50,78 @@ class CityCard extends React.Component {
                 <dt>Sunset:</dt>
                 <dd>20:58</dd>
               </dl>
+            </div>
+            <div className="divider" />
+            <div className="city-card__forecasts">
+              <h2 className="city-card__forecasts-header">Forecast for 7 days</h2>
+              <ul className="forecast">
+                <li className="forecast__day">
+                  <div className="forecast__date">Wed 19 Jul</div>
+                  <div className="forecast__sky-state"><span className="meteocons" data-icon="S" /></div>
+                  <div className="forecast__temperature">
+                    <span className="forecast__temperature-day">19.1 °C</span>
+                    <span className="forecast__temperature-night">17.4 °C</span>
+                  </div>
+                  <div className="forecast__natural-phenomen">light rain</div>
+                </li>
+                <li className="forecast__day">
+                  <div className="forecast__date">Thu 20 Jul</div>
+                  <div className="forecast__sky-state"><span className="meteocons" data-icon="T" /></div>
+                  <div className="forecast__temperature">
+                    <span className="forecast__temperature-day">19.1 °C</span>
+                    <span className="forecast__temperature-night">17.4 °C</span>
+                  </div>
+                  <div className="forecast__natural-phenomen">light rain</div>
+                </li>
+                <li className="forecast__day">
+                  <div className="forecast__date">Fri 21 Jul</div>
+                  <div className="forecast__sky-state"><span className="meteocons" data-icon="U" /></div>
+                  <div className="forecast__temperature">
+                    <span className="forecast__temperature-day">19.1 °C</span>
+                    <span className="forecast__temperature-night">17.4 °C</span>
+                  </div>
+                  <div className="forecast__natural-phenomen">light rain</div>
+                </li>
+                <li className="forecast__day">
+                  <div className="forecast__date">Sat 22 Jul</div>
+                  <div className="forecast__sky-state"><span className="meteocons" data-icon="W" /></div>
+                  <div className="forecast__temperature">
+                    <span className="forecast__temperature-day">19.1 °C</span>
+                    <span className="forecast__temperature-night">17.4 °C</span>
+                  </div>
+                  <div className="forecast__natural-phenomen">light rain</div>
+                </li>
+                <li className="forecast__day">
+                  <div className="forecast__date">Sun 23 Jul</div>
+                  <div className="forecast__sky-state"><span className="meteocons" data-icon="V" /></div>
+                  <div className="forecast__temperature">
+                    <span className="forecast__temperature-day">19.1 °C</span>
+                    <span className="forecast__temperature-night">17.4 °C</span>
+                  </div>
+                  <div className="forecast__natural-phenomen">light rain</div>
+                </li>
+                <li className="forecast__day">
+                  <div className="forecast__date">Mon 24 Jul</div>
+                  <div className="forecast__sky-state"><span className="meteocons" data-icon="X" /></div>
+                  <div className="forecast__temperature">
+                    <span className="forecast__temperature-day">19.1 °C</span>
+                    <span className="forecast__temperature-night">17.4 °C</span>
+                  </div>
+                  <div className="forecast__natural-phenomen">light rain</div>
+                </li>
+                <li className="forecast__day">
+                  <div className="forecast__date">Tue 25 Jul</div>
+                  <div className="forecast__sky-state"><span className="meteocons" data-icon="Y" /></div>
+                  <div className="forecast__temperature">
+                    <span className="forecast__temperature-day">19.1 °C</span>
+                    <span className="forecast__temperature-night">17.4 °C</span>
+                  </div>
+                  <div className="forecast__natural-phenomen">light rain</div>
+                </li>
+              </ul>
+            </div>
+            <div className="city-card__action">
+              <button className="city-card__forecast-btn">Forecast for 7 days</button>
             </div>
           </Paper>
         </div>
